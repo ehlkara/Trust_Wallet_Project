d8d0637bb58761f1b1c007bb7d43b6814833c96d
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2022-01-23 15:24:22 +0300

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:trust_wallet/wallets/bitcoin_transaction.dart';
+import 'package:trust_wallet/wallets/ethereum.dart';
+import 'package:trust_wallet/wallets/private_key_validation.dart';
+import 'package:trust_wallet/wallets/tron.dart';
+import 'package:trust_wallet/wallets/bitcoin_address.dart' as bitcoin;
+import 'package:trust_wallet_core/flutter_trust_wallet_core.dart';
+
+List<String> logs = [];
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Example(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Example extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ExampleState createState() => _ExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExampleState extends State<Example> {
+  late HDWallet wallet;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    FlutterTrustWalletCore.init();
+    super.initState();
+    String mnemonic = "rent craft script crucial item someone dream federal notice page shrug pipe young hover duty"; // 有测试币的 tron地址
+    wallet = HDWallet.createWithMnemonic(mnemonic);
+  }
+
+  Widget _exampleItem({
+    required String name,
+    required WidgetBuilder builder,
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: builder));
+      },
+      child: Text(name),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('wallet core example app'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text(wallet.mnemonic()),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  _exampleItem(
+                    name: 'Ethereum',
+                    builder: (_) {
+                      return Ethereum(wallet);
+                    },
+                  ),
+                  _exampleItem(
+                    name: 'Bitcoin Address',
+                    builder: (_) {
+                      return bitcoin.BitcoinAddress(wallet);
+                    },
+                  ),
+                  _exampleItem(
+                    name: 'Bitcoin Transaction',
+                    builder: (_) {
+                      return BitcoinTransaction(wallet);
+                    },
+                  ),
+                  _exampleItem(
+                    name: 'Tron',
+                    builder: (_) {
+                      return Tron(wallet);
+                    },
+                  ),
+                  _exampleItem(
+                    name: 'PrivateKey.isValid(a,b)',
+                    builder: (_) {
+                      return PrivateKeyValidation(wallet);
+                    },
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              onPressed: () async {
+                wallet.delete();
+                wallet = HDWallet();
+                setState(() {});
+              },
+              child: Text("gen"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
